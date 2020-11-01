6e126f0889f943747759a32f666cd2a9d1f39e01
Sebastian Martino <sbfmartino@gmail.com>
2020-11-01 15:23:48 -0500

    added basic lookup table

--- src/main.c
+++ src/main.c
@@ -1,28 +1,57 @@
-#include <atmel_start.h>
+// #include <atmel_start.h>
+#include <stdio.h>
+#include <string.h>
+
+
+
+#define TABLE_SIZE 2
+typedef struct {
+	char* encoded;
+	char* decoded;
+} translation;
+
+translation lookup[TABLE_SIZE];
+
+void create_table(){
+	lookup[0].decoded = "A";
+	lookup[0].encoded = ".-";
+
+	lookup[1].decoded = "B";
+	lookup[1].encoded = "-...";
+}
+
+char* getEncoded(char* dec){
+	for (int i = 0; i < TABLE_SIZE; i++){
+		if(lookup[i].decoded == dec){
+			return lookup[i].encoded;
+		}
+	}
+	return "Not Found";
+}
+
+char* getDecoded(char* enc){
+	for (int i = 0; i < TABLE_SIZE; i++){
+		if(strcmp(enc, lookup[i].encoded) == 0){
+			return lookup[i].decoded;
+		}
+	}
+	return "Not Found";
+}
+
+
+
 
 int main(void)
 {
 	/* Initializes MCU, drivers and middleware */
-	atmel_start_init();
+	// atmel_start_init();
+
+	create_table();
+	printf("Encoded value of A: %s\n", getEncoded("A"));
+	printf("Decoded value of .-: %s\n", getDecoded(".-"));
+
 
 	/* Replace the code below with your application code */
 
-	/* Set pin PB30 to output */
-	REG_PORT_DIR1 |= (1<<30);
 
-	/* Make it blink! */
-	while (1) {
-		REG_PORT_OUT1 &= ~(1<<30);
-		delay_ms(100);
-		REG_PORT_OUT1 |= (1<<30);
-		delay_ms(100);
-		REG_PORT_OUT1 &= ~(1<<30);
-		delay_ms(100);
-		REG_PORT_OUT1 |= (1<<30);
-		delay_ms(100);
-		REG_PORT_OUT1 &= ~(1<<30);
-		delay_ms(100);
-		REG_PORT_OUT1 |= (1<<30);
-		delay_ms(1000);
-	}
 }
